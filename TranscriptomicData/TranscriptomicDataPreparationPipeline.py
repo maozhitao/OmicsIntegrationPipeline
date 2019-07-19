24e6230be2a6a5bb291a3010d3b42d6b5750e39c
bigghost2054 <cetan@ucdavis.edu>
2019-07-18 22:09:14 -0700

    Update TranscriptomicDataPreparationPipeline.py

--- TranscriptomicData/TranscriptomicDataPreparationPipeline.py
+++ TranscriptomicData/TranscriptomicDataPreparationPipeline.py
@@ -2,3 +2,10 @@ import MicroarrayDataPreparationPipeline.MicroarrayDataPreparationPipeline
 import SequencingDataPreparationPipeline.SequencingDataPreparationPipeline
 import DataPostprocessingPipeline.DataPostprocessingPipeline
 import DataValidationPipeline.DataValidationPipeline
+
+class TranscriptomicDataPreparationPipeline:
+    def __init__(self):
+        self.microarray_data_preparation_pipeline = MicroarrayDataPreparationPipeline.MicroarrayDataPreparationPipeline.MicroarrayDataPreparationPipeline()
+        self.sequencing_data_preparation_pipeline = SequencingDataPreparationPipeline.SequencingDataPreparationPipeline.SequencingDataPreparationPipeline()
+        self.data_postprocessing_pipeline = DataPostprocessingPipeline.DataPostprocessingPipeline.DataPostprocessingPipeline()
+        self.data_validation_pipeline = DataValidationPipeline.DataValidationPipeline.DataValidationPipeline()
