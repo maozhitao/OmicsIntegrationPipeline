42438167a70847926971654a1dbb0f9e03e2bdbc
fangzhouli <fzli0805@gmail.com>
2019-12-18 15:24:38 -0800

    updated processing/ and processing_pipeline.py

--- TranscriptomicPipelines/postprocessing/p_module_template.py
+++ TranscriptomicPipelines/postprocessing/p_module_template.py
@@ -1,19 +1,18 @@
-class PostprocessingModule:
+class PostprocessingModule(object):
     def __init__(self, owner):
         self.owner = owner
-        
+
     def get_parameter_set(self):
         return self.owner.get_parameter_set()
-        
+
     def get_t_compendium_collections(self):
         return self.owner.get_t_compendium_collections()
-        
+
     def get_m_compendium(self):
         return self.owner.get_m_compendium()
-        
+
     def get_s_compendium(self):
         return self.owner.get_s_compendium()
-        
 
 class PostprocessingSubModule(PostprocessingModule):
     def __init__(self, owner):
--- TranscriptomicPipelines/postprocessing_pipeline.py
+++ TranscriptomicPipelines/postprocessing_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+
 if (sys.version_info < (3, 0)):
     sys.path.insert(0, "postprocessing")
     import concatenation
@@ -13,22 +14,22 @@ else:
 
 class PostprocessingPipeline(p_module_template.PostprocessingModule):
     def __init__(self, owner):
-        self.owner = owner
+        self.owner              = owner
         self.data_concatenation = concatenation.Concatenation(self)
-        self.data_imputation = imputation.Imputation(self)
+        self.data_imputation    = imputation.Imputation(self)
         self.data_normalization = normalization.Normalization(self)
-        
+
         self.configure_parameter_set()
-        
+
     def configure_parameter_set_all(self):
         self.configure_parameter_set()
         self.data_concatenation.configure_parameter_set()
         self.data_imputation.configure_parameter_set()
         self.data_normalization.configure_parameter_set()
-        
+
     def configure_parameter_set(self):
         pass
-        
+
     def run_postprocessing_pipeline(self):
         self.data_concatenation.concat_compendium()
         self.data_imputation.impute_data_matrix()
