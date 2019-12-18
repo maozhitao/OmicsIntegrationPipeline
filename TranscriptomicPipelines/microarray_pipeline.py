567419cc43a2fc4b5aaf20d64d09f3c1975a9fbb
fangzhouli <fzli0805@gmail.com>
2019-12-18 14:32:43 -0800

    minor fix on format

--- TranscriptomicPipelines/microarray_pipeline.py
+++ TranscriptomicPipelines/microarray_pipeline.py
@@ -8,15 +8,10 @@ import microarray.m_module_template as m_module_template
 class MicroarrayPipeline(m_module_template.MicroarrayModule):
     def __init__(self, owner, m_query_id, m_compendium):
         self.owner = owner
-        
-        self.m_query_id = m_query_id
-        self.m_compendium = m_compendium
-        
-        self.m_data_retrieval = m_data_retrieval.MicroarrayRetrieval(self)
+
+        self.m_query_id         = m_query_id
+        self.m_compendium       = m_compendium
+        self.m_data_retrieval   = m_data_retrieval.MicroarrayRetrieval(self)
         self.m_value_extraction = m_value_extraction.MicroarrayExtraction(self)
-        self.m_sample_mapping = m_sample_mapping.MicroarraySampleMapping(self)
-        self.m_gene_mapping = m_gene_mapping.MicroarrayGeneMapping(self)
-        
-    
-        
-        
+        self.m_sample_mapping   = m_sample_mapping.MicroarraySampleMapping(self)
+        self.m_gene_mapping     = m_gene_mapping.MicroarrayGeneMapping(self)
